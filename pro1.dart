e412db7f4f9b99e59999eaba8494f98b7287c04c
prerana <147192847+Prerana2923@users.noreply.github.com>
2024-08-15 22:35:24 +0530

    Ifelsestatement
    If else

---
+++ pro1.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  var x = 10;
+  
+  if(x%2==0){
+    print("$x Is a Even Number");
+
+  }else{
+    print( "$x is not even number ");
+  }
+}
---
+++ pro3.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  int age = 19;
+
+  if(age>=18){
+
+    print("sage  you can cast a vote");
+  }else{
+     print("sage  you can't cast a vote");
+
+  }
+}
